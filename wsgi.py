76d46425dc011afb126f3e3eecdb887a1b2a760a
V.Akhil Sathwik <138143460+akhils0810@users.noreply.github.com>
2025-03-30 18:35:42 +0530

    wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,4 @@
-import sys
-import os
+from app import app
 
-# Add your project directory to the sys.path
-project_home = os.path.expanduser('~/GHScheduler')
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
-
-# Import your app from app.py
-from app import application
-
-# This is the WSGI entry point that Gunicorn will use
-app = application 
+if __name__ == "__main__":
+    app.run()
